289d927c7f4f6d8db15a0c7cbbab90468c3d7651
Damian Ka <damiankiszka@o2.pl>
2019-09-07 15:25:48 +0200

    data base connection

---
+++ src/main/java/com/sda/project/wypozyczalnia/config/Config.java
@@ -0,0 +1,33 @@
+package com.sda.project.wypozyczalnia.config;
+
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public DataSource getDataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/wypozyczalnia?serverTimezone=Europe/Warsaw");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("Hakunamatata1");
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        return dataSourceBuilder.build();
+
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+
+}
