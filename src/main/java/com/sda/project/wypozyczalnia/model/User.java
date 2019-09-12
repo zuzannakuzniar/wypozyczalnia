18bdb03b00efbe97fca9065978034eb84486a584
Damian Ka <damiankiszka@o2.pl>
2019-09-12 19:31:43 +0200

    ok

--- src/main/java/com/sda/project/wypozyczalnia/model/User.java
+++ src/main/java/com/sda/project/wypozyczalnia/model/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
 
     private String name;
