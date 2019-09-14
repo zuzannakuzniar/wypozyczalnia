87262b2aebbb3f5996fb713df6a3907f6a5a820c
Robert Kareta <robert.kareta@wp.pl>
2019-09-14 09:53:49 +0200

    ref

--- src/main/java/com/sda/project/wypozyczalnia/model/Car.java
+++ src/main/java/com/sda/project/wypozyczalnia/model/Car.java
@@ -5,7 +5,6 @@ import com.sda.project.wypozyczalnia.extras.Status;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
