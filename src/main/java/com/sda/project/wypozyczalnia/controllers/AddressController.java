71d57a878aaf4c232abd5a2cde8b9636a66b90ac
Damian Ka <damiankiszka@o2.pl>
2019-09-19 13:12:11 +0200

    addind address controller and services and repository, and modificated websecurity

---
+++ src/main/java/com/sda/project/wypozyczalnia/controllers/AddressController.java
@@ -0,0 +1,60 @@
+package com.sda.project.wypozyczalnia.controllers;
+
+import com.sda.project.wypozyczalnia.model.Address;
+import com.sda.project.wypozyczalnia.services.AddressService;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addresses/")
+public class AddressController {
+
+    private AddressService addressService;
+
+    public AddressController(AddressService addressService){
+        this.addressService=addressService;
+    }
+
+    @PostMapping()
+    public Address addNewAddress(@RequestBody Address addressForm) {
+        Address savedAddress = addressService.addNewAddress(addressForm);
+        return savedAddress;
+    }
+
+    @GetMapping()
+    public List<Address> getAllAddresses() {
+        return addressService.getAllAddresses();
+    }
+
+    @GetMapping("/{id}")
+    public Address getOneAddressById(@PathVariable("id") Long id) {
+        return addressService.getOneAddressById(id);
+    }
+
+    @GetMapping("/{city}")
+    public List<Address> getCarsByBrand(@PathVariable("city") String city){
+        return addressService.findByCity(city);
+    }
+
+
+
+    @PutMapping("/{id}")
+    public Address updateAddressById(@PathVariable("id") Long id, @RequestBody Address addressForm) {
+        return addressService.updateAddressById(id, addressForm);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAddressById(@PathVariable("id") Long id) {
+        addressService.deleteAddressById(id);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity handleObjectNotFoundException(ObjectNotFoundException onfe) {
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/com/sda/project/wypozyczalnia/services/AddressService.java
@@ -0,0 +1,58 @@
+package com.sda.project.wypozyczalnia.services;
+
+import com.google.common.collect.Lists;
+import com.sda.project.wypozyczalnia.dao.AddressRepository;
+import com.sda.project.wypozyczalnia.model.Address;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressService {
+
+    private AddressRepository addressRepository;
+
+    public Address addNewAddress(Address address){
+        return addressRepository.save(createNewAddress(address));
+    }
+
+
+
+    public List<Address> getAllAddresses(){
+        return Lists.newArrayList(addressRepository.findAll());
+    }
+
+    public List<Address> findByCity(String city) {
+        return addressRepository.findByCity(city);
+    }
+
+    public Address getOneAddressById(Long id){
+        return addressRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(id, Address.class.getName()));
+    }
+
+    public Address updateAddressById(Long id, Address address){
+        Address address1 = addressRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(id, Address.class.getName()));
+        address1.setCity(address.getCity());
+        address1.setStreet(address.getStreet());
+
+        return address1;
+    }
+    public Address deleteAddressById (Long id) {
+        Address address = addressRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(id, Address.class.getName()));
+
+        addressRepository.deleteById(id);
+        return address;
+    }
+
+    private Address createNewAddress(Address address) {
+        Address result = new Address();
+        result.setCity(address.getCity());
+        result.setStreet(address.getStreet());
+
+        return result;
+    }
+}
